10ef5fe5b088f92d88af83b1d5a1bcde65f9b3ee
Armando Ferrara <armando@ferrara.link>
2020-12-09 18:12:53 +0100

    test deploy

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -6,9 +6,6 @@ import { natsWrapper } from './nats-wrapper';
 
 const start = async () => {
 
-    
-
-    
     if(!process.env.JWT_KEY){
         throw new Error('JWT_KEY must be defined');
     }
