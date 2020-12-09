452d747c7696e05e2cf818bee3a6e2108e4c5759
Armando Ferrara <armando@ferrara.link>
2020-12-09 22:23:03 +0100

    TESTING CI/CD

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -4,6 +4,7 @@ import { currentUser } from '@armando1514-ticket-system/common';
 
 const router = express.Router();
 
+
 router.get('/api/users/currentuser',currentUser, (req, res) => {
     res.send({currentUser: req.currentUser || null});
 });
